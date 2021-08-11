08abaa2b9decb54fc2c86ae2d57b71e7e5f099b5
Leonardo Calderon <jleonardoc@MacBook-Air-de-Leonardo.local>
2021-08-11 01:31:53 -0500

    Adding Changes to Q-Dino

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@
 """
 
 import pygame, sys, os
-from math import sin
 from random import randint
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute, Aer
 
@@ -20,6 +19,9 @@ h_gate_color = pygame.Color(25,182,115)
 cnot_gate_color = pygame.Color(235,85,15)
 text_color = pygame.Color(250,250,240)
 label_color = pygame.Color(60,23,134)
+plate_color = pygame.Color(45,94,46)
+plate_color_inside = pygame.Color(85,194,46)
+ocean_color = pygame.Color(25,112,245) 
 
 dino_image = pygame.image.load('dino.png')
 dino_image = pygame.transform.scale(dino_image, (80, 80))
@@ -31,15 +33,18 @@ cactus_crop = [(0, 0, 34, 80),
                (101, 0, 31, 80),
                (132, 0, 78, 80)]
 
-obstacles = [None, None, None, None, None, randint(0,len(cactus_crop)-1), 
+main_font = pygame.font.Font(os.path.join("pixeboy-font", 'Pixeboy-z8XGD.ttf'), 30)
+obstacles = [None, None, None, None, None, None, None, None, None, None,
+             None, None, None, None, None, None, None, None, None, None,
+             None, None, None, None, None, None, None, None, None, None,
+             None, None, None, None, None, None, None, None, None, None,
+             None, None, None, None, None, randint(0,len(cactus_crop)-1), 
              None, None, None, randint(0,len(cactus_crop)-1), None, None,
              None, None, None, None, None, randint(0,len(cactus_crop)-1),
-             None, None, None, None, randint(0,len(cactus_crop)-1)]
-step = 0
-
-main_font = pygame.font.Font(os.path.join("pixeboy-font", 'Pixeboy-z8XGD.ttf'), 30)
+             None, None, None, None, randint(0,len(cactus_crop)-1), -1]
 
 levels = [
+    ['x', 'cnot', 'cnoti', 'h','x','cnot'],
     ['x'],
     ['x'],
     ['x', 'x'],
@@ -47,21 +52,29 @@ levels = [
     ['h'],
     ['x', 'h']]
 
+step = 0
 level = 0
 gates = levels[level]
 applied_gates = [[], [], []]
 active = 0
 score = 0
 high = 0
-time = 800
+time = 80
 stamp = 0
 living = True
+obstacle_separation = 70
+h_velocity = 0.01
 
 dino_x, dino_y = 10, 320
 cactus_x, cactus_y = 10, 315
-state = 'RUNNING'
-pending_steps = 0
-initial_step = 0
+state = 'IDLE'
+
+pos = 0
+spaces = 0
+jump = [[(4 * x - 0.05 * x * x) for x in range(36)],
+        [(2.1 * x - 0.013 * x * x) for x in range(72)],
+        [(1.5 * x - 0.00625 * x * x) for x in range(108)],
+        [(1.05 * x - 0.003125 * x * x) for x in range(144)]]
 
 def main(argv):
     pygame.display.set_caption('Q-Dino')
@@ -71,30 +84,40 @@ def main(argv):
         draw_gates()
         draw_score()
         draw_time()
-        
-        for obs in range(int(step), len(obstacles)):
-            if obstacles[obs] != None:
-                main_window.blit(cactus_image, 
-                                 (cactus_x + ((obs - step) * 70), cactus_y), 
-                                 cactus_crop[obstacles[obs]])
+        draw_obstacles()
         draw_dino()
         event_listener()
         validate_cnot()
+        if level == len(levels) and state == 'RUNNING':
+            init_all()
+        if int(step) == len(obstacles):
+            init_all()
         pygame.display.update()
+        pygame.time.wait(1)
+
+def splash_screen():
+    s = pygame.Surface((1000,750), pygame.SRCALPHA)
+    s.fill((255,255,255,128))
+    main_window.blit(s, (0,0))
+    game_over = main_font.render("GAME OVER", 0, label_color)
+    main_window.blit(game_over, (400, 300))
+    pygame.display.update()
+    pygame.time.wait(3000)
 
 def event_listener():
-    global active, stamp, level, gates, applied_gates
+    global active, stamp, level, gates, applied_gates, state
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
+            exit_game()
+        elif event.type == pygame.KEYDOWN and state in ('RUNNING', 'IDLE'):
             if stamp == 0:
                 stamp = pygame.time.get_ticks()
+                state = 'RUNNING'
             if event.key == pygame.K_DOWN:
                 if active == 0:
-                    active = 1
-                    applied_gates[0].append(gates.pop())
+                    if len(gates) > 0:
+                        active = 1
+                        applied_gates[0].append(gates.pop())
                 elif active < len(applied_gates):
                     applied_gates[active].append(applied_gates[active-1].pop())
                     active += 1
@@ -103,8 +126,9 @@ def event_listener():
                     gates.append(applied_gates[2].pop())
             elif event.key == pygame.K_UP:
                 if active == 0:
-                    active = len(applied_gates)
-                    applied_gates[-1].append(gates.pop())
+                    if len(gates) > 0:
+                        active = len(applied_gates)
+                        applied_gates[-1].append(gates.pop())
                 elif active == 1:
                     active = 0
                     gates.append(applied_gates[0].pop())
@@ -116,10 +140,12 @@ def event_listener():
                     gates[-1] = 'cnoti'
                 elif active == 0 and gates[-1] == 'cnoti':
                     gates[-1] = 'cnot'
-                elif applied_gates[active-1][-1] == 'cnot':
-                    applied_gates[active-1][-1] = 'cnoti'
-                elif applied_gates[active-1][-1] == 'cnoti':
-                    applied_gates[active-1][-1] = 'cnot'
+                elif len(applied_gates) > 0:
+                    if len(applied_gates[active - 1]) > 0:
+                        if applied_gates[active-1][-1] == 'cnot':
+                            applied_gates[active-1][-1] = 'cnoti'
+                        elif applied_gates[active-1][-1] == 'cnoti':
+                            applied_gates[active-1][-1] = 'cnot'
             elif event.key == pygame.K_SPACE:
                 if active != 0:
                     for i in range(len(applied_gates)):
@@ -132,7 +158,8 @@ def event_listener():
                     execute_dino()
                     if living:
                         level+=1
-                    gates = levels[level]
+                    if level < len(levels):
+                        gates = levels[level]
                     applied_gates = [[], [], []]
                     stamp = 0
 
@@ -215,24 +242,60 @@ def draw_circuit():
             x += 40
 
 def draw_dino():
-    global state, step, initial_step, pending_steps
-    if state == 'RUNNING' or state == 'DUCKING':
+    global state, step, pos, score, high
+    if state == 'IDLE':
+        main_window.blit(dino_image, (dino_x, dino_y))
+    elif state == 'RUNNING' or state == 'DUCKING':
+        prev_step = step
+        step += h_velocity
+        if int(prev_step) != int(step):
+            score += 1
+            high = max(high, score)
         main_window.blit(dino_image, (dino_x, dino_y))
     elif state == 'JUMPING':
-        step += 0.01
-        main_window.blit(dino_image, (dino_x,
-                                      dino_y - sin((step - initial_step)/(pending_steps-initial_step)) * 200))
-        if step >= initial_step + (pending_steps - initial_step) / 2:
+        step += h_velocity * (spaces + 1)
+        main_window.blit(dino_image, (dino_x,dino_y - jump[spaces][pos]))
+        pos += spaces + 1
+        if pos >= len(jump[spaces]):
             state = 'FALLING'
+            pos = len(jump[spaces]) - 1
     elif state == 'FALLING':
-        step += 0.01
-        main_window.blit(dino_image, (dino_x,
-                                      dino_y - sin((pending_steps - step)/(pending_steps-initial_step)) * 200))
-        if step >= pending_steps:
-            step = pending_steps
-            pending_steps = 0
-            initial_step = 0
+        step += h_velocity * (spaces + 1)
+        main_window.blit(dino_image, (dino_x,dino_y - jump[spaces][pos]))
+        pos -= spaces + 1
+        if pos <= 0:
             state = 'RUNNING'
+            pos = 0
+            
+def draw_obstacles():
+    for obs in range(int(step) - 1, len(obstacles)):
+        x = cactus_x + ((obs - step) * obstacle_separation) - 15
+        y = cactus_y + 60
+        if x > -obstacle_separation * 2:
+            if obstacles[obs] == -1 and int(step) > 0:
+                pygame.draw.polygon(main_window, ocean_color,
+                                    [[x,y],[x+20,y+25],[x+1288, y+25],[x+1268,y]])
+            else:
+                pygame.draw.polygon(main_window, plate_color,
+                                    [[x,y],[x+20,y+25],[x+88, y+25],[x+68,y]])
+                pygame.draw.polygon(main_window, plate_color_inside,
+                                    [[x+12,y+5],[x+25,y+20],[x+78, y+20],[x+63,y+5]])
+                if obstacles[obs] != None and obstacles[obs] != -1:
+                    xpos = cactus_x + ((obs - step) * obstacle_separation);
+                    ypos = cactus_y
+                    main_window.blit(cactus_image, 
+                                    (xpos,
+                                     ypos), 
+                                    cactus_crop[obstacles[obs]])
+                    validate_collision(xpos, ypos, cactus_crop[obstacles[obs]])
+
+def validate_collision(xpos, ypos, obs):
+    if dino_x + 65 >= xpos and dino_x + 65 <= xpos + obs[2]:
+        if state == "RUNNING" or state == "DUCKING":
+            init_all()
+    if dino_x >= xpos and dino_x <= xpos + obs[2]:
+        if state == "RUNNING" or state == "DUCKING":
+            init_all()
             
 def validate_cnot():
     global active
@@ -270,27 +333,60 @@ def execute_dino():
     play_dino(measurement_result)
     
 def play_dino(measurement_result):
-    global step, score, high, state, pending_steps, initial_step
+    global step, score, high, state, pending_steps, pos, spaces
     action = max(measurement_result, key=measurement_result.get)
-    print(action)
     # The first classical bit indicates if the Q-Dino will jump or duck
     # 0 = Jump, 1 = Duck
     # The second and third bit indicates the number of spaces Q-Dino will move
     # 00 = Run 1 space, 01 = Jump 2 spaces, 10 = Jump 3 spaces, 11 = Jump 4 spaces 
-    print(action[0])
     if(action[0] == '0'):
         state = "JUMPING"
     else:
         state = "DUCKING"
-    print(state)
     
-    spaces = int(action[1:], 2) + 1
-    print(spaces)
-    # step += spaces
-    initial_step = step
-    pending_steps = step + spaces
-    score += 10 * spaces
+    spaces = int(action[1:], 2)
+    score += 10 * (spaces + 1)
     high = max(high, score)
+    pos = 0
+    
+    print(state + " kind " + str(spaces + 1))
+
+def init_all():
+    global step, levels, level, gates, applied_gates, active, score, time, \
+    stamp, living, obstacle_separation, h_velocity, state, pos, spaces, \
+    obstacles
+    levels = [
+        ['x'],
+        ['x'],
+        ['x', 'x'],
+        ['x', 'x'],
+        ['h'],
+        ['x', 'h']]
+    
+    obstacles = [None, None, None, None, None, randint(0,len(cactus_crop)-1), 
+             None, None, None, randint(0,len(cactus_crop)-1), None, None,
+             None, None, None, None, None, randint(0,len(cactus_crop)-1),
+             None, None, None, None, randint(0,len(cactus_crop)-1), -1]
+
+    step = 0
+    level = 0
+    gates = levels[level]
+    applied_gates = [[], [], []]
+    active = 0
+    score = 0
+    time = 80
+    stamp = 0
+    living = True
+    obstacle_separation = 70
+    h_velocity = 0.01
+    state = 'IDLE'
+    pos = 0
+    spaces = 0
+    splash_screen()
+
+def exit_game():
+    pygame.quit()
+    sys.exit()
         
 if __name__ == "__main__":
     main(sys.argv)
